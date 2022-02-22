a2a2690739093dcd132586b640f562c2c5af2ae6
장효진 <90826012+hyojin-dev@users.noreply.github.com>
2022-02-22 17:38:42 +0900

    Update ChatRoomService.java

--- WebSocket/chat/service/ChatRoomService.java
+++ WebSocket/chat/service/ChatRoomService.java
@@ -7,7 +7,6 @@ import com.cdp.tdp.domain.User;
 import com.cdp.tdp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,33 +24,33 @@ public class ChatRoomService {
         return chatRoomRepository.findAll();
     }
 
-    public ChatRoom createRoom(ChatRoomDTO chatRoomDTO, Long id) {
-        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("no such user"));
-        ChatRoom room = new ChatRoom(chatRoomDTO, user);
+    public ChatRoom createRoom(ChatRoomDTO chatRoomDTO,Long id)  {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다. userId = " + id));
+        ChatRoom room=new ChatRoom(chatRoomDTO,user);
         chatRoomRepository.save(room);
         return room;
     }
 
-    public ChatRoom getRoom(Long roomId) {
-        return chatRoomRepository.findById(roomId).orElseThrow(
-                () -> new NullPointerException("해당 채팅방이 존재하지 않습니다")
+    public ChatRoom getRoom(Long id) {
+        return chatRoomRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 채팅방이 존재하지 않습니다. ChatRoodId = "+ id)
         );
     }
 
-    public boolean checkMyRoom(Long roomId, Long nowUserId) {
-        ChatRoom chatRoom = getRoom(roomId);
-        if (chatRoom.getUser().getId().equals(nowUserId)) {
+    public boolean checkmyroom(Long room_id,Long user_id){
+       ChatRoom chatRoom=chatRoomRepository.findById(room_id).orElseThrow(
+               ()-> new NullPointerException("해당 채팅방이 존재하지 않습니다. ChatRoodId = "+ room_id)
+       );
+       if(chatRoom.getUser().getId().equals(user_id)){
             return true;
-        }
-        return false;
+       }
+       return false;
     }
 
     @Transactional
-    public String deleteRoom(Long id, Long nowUserId) {
-        if (!id.equals(nowUserId)) {
-            throw new AccessDeniedException("채팅방을 삭제할 권한이 없습니다.");
-        }
+    public void deleteRoom(Long id) {
+        log.info("delete 전");
         chatRoomRepository.deleteById(id);
-        return "채팅방 삭제가 완료되었습니다.";
+        log.info("delete");
     }
 }
